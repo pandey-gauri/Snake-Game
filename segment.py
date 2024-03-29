997514bca1a9280304c8d386014f3a9b10567ce4
pandey-gauri <gauri22pandey@gmail.com>
2024-03-29 21:49:14 +0530

    Files for the Snake Game

---
+++ segment.py
@@ -0,0 +1,12 @@
+# Method to create snake segments
+
+# segment_1= Turtle("square")
+# segment_1.color("white")
+
+# segment_2= Turtle("square")
+# segment_2.color("white")
+# segment_2.goto(-20,0)
+
+# segment_3= Turtle("square")
+# segment_3.color("white")
+# segment_3.goto(-40,0)
